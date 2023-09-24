085deb84d6332ed0e16e3e39b252fa2f06e01af1
qiboda <qiboda00@gmail.com>
2023-09-24 10:55:57 +0800

    add tag registry example

--- crates/layertag/examples/tag.rs
+++ crates/layertag/examples/tag.rs
@@ -1,5 +1,8 @@
 use bevy::{prelude::*, reflect::TypePath};
-use layertag::layertag::LayerTag;
+use layertag::{
+    layertag::LayerTag,
+    registry::{FromTagRegistry, LayerTagRegistry},
+};
 use std::{fmt::Debug, marker::PhantomData};
 
 use layertag_derive::{layer_tag, LayerTag};
@@ -10,6 +13,12 @@ pub struct TestTags {}
 
 static TAG_A: &str = "a";
 static TAG_B: &str = "b";
+
+impl FromTagRegistry for TestTags {
+    fn from_tag_registry() -> Self {
+        Self {}
+    }
+}
 
 #[derive(LayerTag, Debug, Clone, Reflect)]
 #[layer_tag(TAG_A, TAG_B, "d")]
@@ -23,9 +32,12 @@ where
 
 fn main() {
     println!("--- test_tags ---");
-    let test_tags = TestTags {};
-    for tag in test_tags.tag().iter() {
-        println!("tag: {:?}", tag);
+    let mut layertag_registry = LayerTagRegistry::default();
+    layertag_registry.register::<TestTags>();
+    if let Some(test_tags) = layertag_registry.get::<TestTags>() {
+        for tag in test_tags.tag().iter() {
+            println!("tag: {:?}", tag);
+        }
     }
     println!("--- generic_tags ---");
     let generic_tags = GenTestTags::<i32> { _data: PhantomData };
