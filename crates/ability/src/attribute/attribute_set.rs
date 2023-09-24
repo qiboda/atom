7185557fb926e87aac7a947b808d4859d4fc76b3
qiboda <qiboda00@gmail.com>
2023-09-24 13:37:50 +0800

    add attribute set component

---
+++ crates/ability/src/attribute/attribute_set.rs
@@ -0,0 +1,13 @@
+use bevy::reflect::Reflect;
+
+use super::{modifier::AttributeModifier, Attribute};
+
+pub trait AttributeSet: Reflect {
+    type AttributeSetEnum;
+
+    fn get_attr_final_value(&self, attribute_set_enum: Self::AttributeSetEnum) -> Option<f32>;
+
+    fn apply_modify(&mut self, modifier: impl AttributeModifier<AttributeSetType = Self>) {
+        modifier.receive_attribute_set(self);
+    }
+}
--- crates/ability/src/attribute/mod.rs
+++ crates/ability/src/attribute/mod.rs
@@ -1,1 +1,14 @@
+use bevy::reflect::Reflect;
 
+pub mod attribute_set;
+pub mod modifier;
+
+// 属性支持范围限定，叠加计算。
+// 属性间支持联系，以及属性间范围限定，以及叠加计算。
+//
+// Note: 暂时不支持任意类型的属性。
+pub trait Attribute: Reflect {
+    fn get_value(&self) -> &f32;
+
+    fn get_value_mut(&mut self) -> &mut f32;
+}
